f1b4daa10c39c7fa51f5903af501a11303881819
Andrei Vaniuk <avaniuk@demandbase.com>
2022-05-03 14:24:37 +0300

    fix

--- app/app.py
+++ app/app.py
@@ -83,7 +83,7 @@ def handle_401(error):
 def hello_world():
     return 'Hello World!'
 
-
-if __name__ == '__main__':
-    app.run()
+#
+# if __name__ == '__main__':
+#     app.run()
 
--- app/clients/postgres_client.py
+++ app/clients/postgres_client.py
@@ -1,4 +1,5 @@
 import itertools
+import logging
 from typing import Dict, List, Any
 
 import sqlalchemy
@@ -195,6 +196,7 @@ class PostgresClient:
         return self.pg_db.query(self._get_queryset(*args, **kwargs).exists()).scalar()
 
     def fetch(self, *args, **kwargs):
+        logging.error(CONFIG.PG_DB)
         return self._get_queryset(*args, **kwargs).first()
 
     def fetch_all(self, *args, **kwargs):
